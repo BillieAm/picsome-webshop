acebca2338fc3b4b95d830dd2bd1a78407df1698
Billie Amit <biliamit@gmail.com>
2022-04-26 19:49:39 +0300

    display icons on image hover

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Image({ img, className }) {
+  const [hovered, setHovered] = useState(false);
+
+  const heartIcon = hovered && <i className="ri-heart-line icon"></i>;
+  const cartIcon = hovered && <i className="ri-add-circle-line icon"></i>;
+
   return (
-    <div className={`${className} image-container`}>
+    <div
+      className={`${className} image-container`}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+    >
       <img alt="" src={img.url} className="image-grid" />
+      {heartIcon}
+      {cartIcon}
     </div>
   );
 }
