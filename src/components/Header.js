c4f47a56348d57739116005ea5d850cf415225d8
Billie Amit <biliamit@gmail.com>
2022-04-28 12:30:26 +0300

    setup cart page and remove items from cart page functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../Context";
 
 function Header() {
+  const { cartItems } = useContext(Context);
+
+  const cartClassName = cartItems.length
+    ? "ri-shopping-cart-fill"
+    : "ri-shopping-cart-line";
+
   return (
     <header>
       <Link to="/">
         <h2>Pic Some</h2>
       </Link>
       <Link to="/cart">
-        <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        <i className={`${cartClassName} ri-fw ri-2x`}></i>
       </Link>
     </header>
   );
--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,17 @@
+import { useContext } from "react";
+import CartItem from "../components/CartItem";
+import { Context } from "../Context";
+
 function Cart() {
+  const { cartItems } = useContext(Context);
+
+  const cartItemElements = cartItems.map((item) => <CartItem item={item} />);
   return (
     <main className="cart-page">
       <h1>Check out</h1>
+      {cartItemElements}
+      <p className="total-cost">Total: </p>
+      <button className="order-button">Place Order</button>
     </main>
   );
 }
