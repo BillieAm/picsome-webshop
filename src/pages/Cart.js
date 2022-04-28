891149de81eaeba32b65b08e6e9320ac26118447
Billie Amit <biliamit@gmail.com>
2022-04-28 12:47:37 +0300

    set the total number display

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,13 +4,19 @@ import { Context } from "../Context";
 
 function Cart() {
   const { cartItems } = useContext(Context);
+  const total = cartItems.length * 5.99;
+  const totalDisplay = total.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
 
   const cartItemElements = cartItems.map((item) => <CartItem item={item} />);
+
   return (
     <main className="cart-page">
       <h1>Check out</h1>
       {cartItemElements}
-      <p className="total-cost">Total: </p>
+      <p className="total-cost">Total: {totalDisplay}</p>
       <button className="order-button">Place Order</button>
     </main>
   );
